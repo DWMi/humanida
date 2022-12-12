048297ef945bee71c39264f7c7d3df84e7f550f3
Hugo Bengtsson <hugo@capellium.se>
2022-12-12 11:17:11 +0100

    Initial server setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieSession from "cookie-session";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Converting all requests from JSON.
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    secret: "mQCBJEfr2zcxuE2zKgX5",
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    sameSite: false,
+    httpOnly: true,
+    secure: false,
+  })
+);
+
+app.listen(port, () => {
+  console.log("Server is running on port: " + port);
+});
