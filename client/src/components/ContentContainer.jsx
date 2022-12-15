7bc946ccca40610785e3407b3b0ef99141047dfe
David w <cf.davidwong@gmail.com>
2022-12-15 15:08:40 +0100

    weird

--- client/src/components/ContentContainer.jsx
+++ client/src/components/ContentContainer.jsx
@@ -1,17 +1,18 @@
 import { Route, Routes } from "react-router";
 import Kompetens from "./Kompetens";
-import Profil from "./profil";
+// import Profil from "./Profil";
 import Startsida from "./Startsida";
+import Login from './Login';
 
 const ContentContainer = () => {
   return (
     <Routes>
       <Route path="/" element={<Startsida />} />
       <Route path="/kompetens/:id" element={<Kompetens />} />
-      <Route path="/profil/:id" element={<Profil/>} />
+      {/* <Route path="/profil/:id" element={<Profil/>} /> */}
       <Route path="/kontakt" element={<h1> Kontakta oss </h1>} />
       <Route path="/min-sida/:id" element={<h1> Min Sida </h1>} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
   );
 };
