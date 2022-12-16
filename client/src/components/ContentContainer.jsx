7fdc65954c98efb5866c4496a618988dc8419ddb
Niki-van <hanko.vangjeli@studio.unibo.it>
2022-12-16 13:47:52 +0100

    kontaktsida-klart2

--- client/src/components/ContentContainer.jsx
+++ client/src/components/ContentContainer.jsx
@@ -3,54 +3,16 @@ import { Route, Routes } from "react-router"
 import Kompetens from "./Kompetens"
 import Startsida from "./Startsida"
 import KontaktSida from "./KontaktSida"
-
-const ContentContainer =()=>{
-
-    return(
-        <Routes>
-            <Route
-                path="/"
-                element={<Startsida/>}
-            />
-            <Route
-                path="/kompetens/:id"
-                element={<Kompetens/>}
-            />
-            <Route
-                path="/profil/:id"
-                element={<h1> Min Profil </h1>}
-            />
-            <Route
-                path="/kontakt"
-                element={< KontaktSida />}
-            />
-            <Route
-                path="/min-sida/:id"
-                element={<h1> Min Sida </h1>}
-            />
-            <Route
-                path="/login"
-                element={<h1> Login </h1>}
-            />
-        </Routes>
-    )
-}
-
-export default ContentContainer
-
-import { Route, Routes } from "react-router";
-import Kompetens from "./Kompetens";
-// import Profil from "./Profil";
-import Startsida from "./Startsida";
-import Login from './Login';
+//import Profil from "./Profil"
+import Login from "./Login"
 
 const ContentContainer = () => {
   return (
     <Routes>
       <Route path="/" element={<Startsida />} />
       <Route path="/kompetens/:id" element={<Kompetens />} />
-      {/* <Route path="/profil/:id" element={<Profil/>} /> */}
-      <Route path="/kontakt" element={<h1> Kontakta oss </h1>} />
+      {/*<Route path="/profil/:id" element={<Profil/>} />*/}
+      <Route path="/kontakt" element={ <KontaktSida/>} />
       <Route path="/min-sida/:id" element={<h1> Min Sida </h1>} />
       <Route path="/login" element={<Login/>} />
     </Routes>
--- client/src/components/RenderUserCard.jsx
+++ client/src/components/RenderUserCard.jsx
@@ -2,41 +2,6 @@ import "./styles/kompetens.css";
 import { Avatar, Card } from "@mantine/core";
 const RenderUserCard = (props) => {
 
-    return (
-        <>
-        {props.user.map((user)=>{
-            return (
-            <Card
-    >
-      <Card.Section>
-     
-          <Avatar
-          component="a"
-          href={"/profil/"+user.ID}
-          target="_blank"
-          src="/"
-          alt="it's me"
-   />
-      </Card.Section>
-
-
-      <p>{user.name}</p>
-            <p>{user.email}</p>
-     
-                      
-    </Card>
- 
-            
-            
-        ) 
-        })}
-
-
-        </>
-    )
-}
-export default RenderUserCard
-
   return (
     <>
       {props.user.map((user) => {
