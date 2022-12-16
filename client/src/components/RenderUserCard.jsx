e0d97e8ee6966e4a33d44c368d8edd2098cd1f83
Hugo Bengtsson <hugo@capellium.se>
2022-12-16 09:54:22 +0100

    Small fixes

--- client/src/components/RenderUserCard.jsx
+++ client/src/components/RenderUserCard.jsx
@@ -1,42 +1,28 @@
-
-
-import './styles/kompetens.css'
-import { Avatar,Card } from '@mantine/core';
+import "./styles/kompetens.css";
+import { Avatar, Card } from "@mantine/core";
 const RenderUserCard = (props) => {
-    return (
-        <>
-        {props.user.map((user)=>{
-            return (
-            <Card
-    >
-      <Card.Section>
-     
-          <Avatar
-          component="a"
-          href={"/profil/"+user.ID}
-          target="_blank"
-          src="/"
-          alt="it's me"
-   />
-      </Card.Section>
+  return (
+    <>
+      {props.user.map((user) => {
+        return (
+          <Card key={user.id} style={{ textAlign: "center" }}>
+            <Card.Section>
+              <Avatar
+                component="a"
+                href={"/profil/" + user.ID}
+                src="/"
+                alt="it's me"
+              />
+            </Card.Section>
 
-
-      <p>{user.name}</p>
-            <p>{user.email}</p>
-     
-                      
-    </Card>
- 
-            
-            
-        ) 
-        })}
-
-        
-
-
-
-        </>
-    )
-}
-export default RenderUserCard
+            <p>{user.name}</p>
+            <a href={`mailto:${user.email}`} style={{ paddingTop: "20px" }}>
+              {user.email}
+            </a>
+          </Card>
+        );
+      })}
+    </>
+  );
+};
+export default RenderUserCard;
--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import './styles/footer.css'
+import React from "react";
+import "./styles/footer.css";
 
 export default function Footer() {
   return (
-    
-    <footer> 
-   
-    <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        
-      <a className='contact' href="/kontakt">
-        <div className="kontakta-link">
-            Kontakta oss
+    <footer>
+      <div
+        className="text-center p-4"
+        style={{
+          backgroundColor: "rgba(0, 0, 0, 0.05)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div>
+          <p className="contact">Anders Personsgatan 18,</p>
+          <p className="contact">416 64 Göteborg</p>
         </div>
-      </a>
-          Copyright © 2022 HUMANIDA 
-      </div> 
-      </footer>
-  )
+        <div>
+          <a className="contact" href="/kontakt">
+            <div className="kontakta-link">Kontakta oss</div>
+          </a>
+          Copyright © 2022 HUMANIDA
+        </div>
+      </div>
+    </footer>
+  );
 }
-    
