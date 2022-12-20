bf7d3d2f6e4cf000467cbc2157cb611132dccd6e
David w <cf.davidwong@gmail.com>
2022-12-20 14:45:20 +0100

    Revert "minor fix"
    This reverts commit 3fad4e0340e1fd3bd88016f079784376ba634ca5.

--- client/src/components/Tags.jsx
+++ client/src/components/Tags.jsx
@@ -1,19 +1,27 @@
-import "./styles/tags.css";
+import './styles/tags.css'
 
-const Tags = (props) => {
-  return props.tags?.map((tag) => {
-    if (tag.ID != null) {
-      return (
-        <a
-          href={`/kompetens/${tag.ID}`}
-          style={{ textDecoration: "none" }}
-          key={tag.ID}
-        >
-          <div className="tagBox">{tag.name}</div>
-        </a>
-      );
-    }
-  });
-};
 
-export default Tags;
+const Tags =(props)=>{
+
+    return(
+     
+        props.tags?.map((tag)=>{
+            return(
+                
+                <a href={`/kompetens/${tag.ID}`} style={{textDecoration:'none'}} key={tag.ID}>
+            
+                    <div className="tagBox"
+                    >
+                        {tag.name}
+                    </div>
+                </a>
+            
+                
+            )
+        })
+      
+        
+    )
+}   
+
+export default Tags
